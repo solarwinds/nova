a6cfdd0bfb69242ba52ab4bced7b72048443b07f
Lukas Holcik <lukas.holcik@solarwinds.com>
2020-11-17 13:07:45 -0600

    Removed nova-images dependency

--- packages/bits/demo/src/components/demo/image/image-docs/image-docs.example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "nui-image-docs-example",
-    templateUrl: "./image-docs.example.component.html",
-})
-
-export class ImageDocsExampleComponent {}
--- packages/bits/demo/src/components/demo/image/image-float/image-float.example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "nui-image-float-example",
-    templateUrl: "./image-float.example.component.html",
-})
-
-export class ImageFloatExampleComponent {}
