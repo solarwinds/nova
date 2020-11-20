17b31d7a473b16ec55af70180964529cf8e8938f
Lukáš Holčík <lukas.holcik@gmail.com>
2020-11-20 14:47:04 -0600

    Feature/nui 5467 circleci bits (#14)
    * Initial circleci configuration file
    
    * Renamed build
    
    * Attempt to fix checkout path
    
    * Removed nova-images dependency
    
    * Removed ts-lint-rules dependency
    
    * Removed aot property
    
    * e2e experiments
    
    * Persist experiment
    
    * Workflow parallelization
    
    * Fixed configuration
    
    * Everything in one job
    
    * Added workflow
    
    * Experimenting with workspace
    
    * Experimenting with workspace #2
    
    * Experimenting with workspace #3
    
    * attach_workspace
    
    * Fixed paths
    
    * Persist build output
    
    * Merged jobs
    
    * Added checkout
    
    * Fixed paths
    
    * Testing e2e
    
    * docker compose experiments
    
    * docker compose experiments #2
    
    * Install sudo
    
    * Changed installation path
    
    * Changed installation path #2
    
    * Fixed docker-compose installation
    
    * Changed docker to machine
    
    * Fixed config file
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * Polishing
    
    * Disabled image tests

--- packages/bits/demo/src/components/demo/image/image-basic/image-basic.example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "nui-image-basic-example",
-    templateUrl: "./image-basic.example.component.html",
-})
-
-export class ImageBasicExampleComponent {}
--- packages/bits/demo/src/components/demo/image/image-watermarked/image-watermarked.example.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: "nui-image-watermarked-example",
-    templateUrl: "./image-watermarked.example.component.html",
-})
-
-export class ImageWatermarkedExampleComponent {}
