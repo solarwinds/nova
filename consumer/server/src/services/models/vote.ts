1125b038bec3d252d97bab9d531fb18d3b644e8f
Baggio Shehadi <baggio.shehadi@solarwinds.com>
2020-12-08 15:09:04 -0600

    Removed rubber ducky app and added last steps for release

--- consumer/server/src/services/models/vote.ts
+++
@@ -1,9 +0,0 @@
-import * as mongoose from "mongoose";
-
-export const voteSchema = new mongoose.Schema({
-    postId: String,
-    userId: String,
-    vote: Number,
-});
-
-export const voteModel = mongoose.model("Vote", voteSchema);
