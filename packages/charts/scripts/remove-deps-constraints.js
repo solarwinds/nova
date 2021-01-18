55bf73a3f734c007bd0011c3bdd2296d503435ca
Baggio Shehadi <baggio.shehadi@solarwinds.com>
2021-01-18 15:52:43 -0600

    Added script to charts to remove deps from lock

---
+++ packages/charts/scripts/remove-deps-constraints.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const lockfile = require("@yarnpkg/lockfile");
+
+
+let yarnFile = fs.readFileSync("yarn.lock", "utf8");
+let json = lockfile.parse(yarnFile);
+
+
+console.log(json);
+
+for (const dependency in json.object) {
+    if(dependency.startsWith("@nova-ui/")) {
+        delete json.object[dependency];
+    }
+}
+
+let fileAgain = lockfile.stringify(json.object);
+
+fs.writeFileSync("yarn.lock", fileAgain);
+// console.log(fileAgain);
