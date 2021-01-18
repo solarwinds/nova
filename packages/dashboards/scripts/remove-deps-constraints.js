c424ad673180165dd78bc84564653d08b4c67e95
Baggio Shehadi <baggio.shehadi@solarwinds.com>
2021-01-18 16:49:15 -0600

    Adding caching to dashboards

---
+++ packages/dashboards/scripts/remove-deps-constraints.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const lockfile = require("@yarnpkg/lockfile");
+
+let yarnFile = fs.readFileSync("yarn.lock", "utf8");
+let json = lockfile.parse(yarnFile);
+
+for (const dependency in json.object) {
+    if (dependency.startsWith("@nova-ui/")) {
+        delete json.object[dependency];
+    }
+}
+
+let fileAgain = lockfile.stringify(json.object);
+
+fs.writeFileSync("yarn.lock", fileAgain);
