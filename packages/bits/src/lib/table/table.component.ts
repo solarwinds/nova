667ca64b48c7a63d08f639664523af8d5160cc62
Baggio Shehadi <baggio.shehadi@solarwinds.com>
2021-02-02 11:16:33 -0600

    Deleted html template and imported CDK_TABLE_TEMPLATE

--- packages/bits/src/lib/table/table.component.ts
+++ packages/bits/src/lib/table/table.component.ts
@@ -1,7 +1,7 @@
 import { Directionality } from "@angular/cdk/bidi";
 import { Platform } from "@angular/cdk/platform";
 import { CdkVirtualForOf } from "@angular/cdk/scrolling";
-import { CdkTable } from "@angular/cdk/table";
+import { CdkTable, CDK_TABLE_TEMPLATE } from "@angular/cdk/table";
 import { DOCUMENT } from "@angular/common";
 import {
     AfterContentInit,
@@ -42,7 +42,9 @@ interface TableRowData {
 /** @dynamic */
 @Component({
     selector: "nui-table, table[nui-table]",
-    templateUrl: "./table.component.html",
+    // We used to have our own template for the table but it broke with Angular release 10 so we are now using this even though it is private so we can be
+    // up to date with the CDK table template.
+    template: CDK_TABLE_TEMPLATE,
     exportAs: "nuiTable",
     host: {
         "class": "nui-table__table",
