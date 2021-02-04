e23062a4de25d8c0b8c90e14ab40d7b6135e7003
kamelevskyi <vladyslav.kamelevskyi@solarwinds.com>
2021-02-04 16:56:59 +0200

    NUI-2825: Form components should not publish valueChanges after blur if value has not changed

--- packages/bits/src/lib/textbox/textbox.component.spec.ts
+++ packages/bits/src/lib/textbox/textbox.component.spec.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component, ViewChild } from "@angular/core";
+import { Component, NO_ERRORS_SCHEMA, ViewChild } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { FormBuilder, FormsModule, ReactiveFormsModule } from "@angular/forms";
 
@@ -9,7 +9,6 @@ import { SpinnerComponent } from "../spinner/spinner.component";
 import { TextboxComponent } from "./textbox.component";
 
 @Component({
-    selector: "textbox-form-component",
     template: `
         <div [formGroup]="form">
             <nui-form-field [control]="form.controls['input']">
@@ -34,15 +33,11 @@ describe("components >", () => {
     describe("textbox >", () => {
 
         let fixture: ComponentFixture<TextboxComponent>;
-        let formFixture: ComponentFixture<TextboxFormComponent>;
         let testComponent: TextboxComponent;
-        let testFormComponent: TextboxFormComponent;
 
         beforeEach(() => {
             TestBed.configureTestingModule({
                 imports: [
-                    CommonModule,
-                    FormsModule,
                     ReactiveFormsModule,
                     NuiFormFieldModule,
                 ],
@@ -55,12 +50,7 @@ describe("components >", () => {
 
             fixture = TestBed.createComponent(TextboxComponent);
             testComponent = fixture.componentInstance;
-
-            formFixture = TestBed.createComponent(TextboxFormComponent);
-            testFormComponent = formFixture.componentInstance;
-
             fixture.detectChanges();
-            formFixture.detectChanges();
         });
 
         describe("general > ", () => {
@@ -70,13 +60,44 @@ describe("components >", () => {
                 testComponent.focus();
                 expect(testComponent.textboxInput.nativeElement).toBe(document.activeElement);
             });
+        });
+    });
+});
 
+describe("components >", () => {
+    describe("textbox >", () => {
+
+        let fixture: ComponentFixture<TextboxFormComponent>;
+        let testComponent: TextboxFormComponent;
+
+        beforeEach(() => {
+            TestBed.configureTestingModule({
+                declarations: [
+                    TextboxComponent,
+                    TextboxFormComponent,
+                    SpinnerComponent,
+                ],
+                imports: [
+                    CommonModule,
+                    FormsModule,
+                    ReactiveFormsModule,
+                    NuiFormFieldModule,
+                ],
+                schemas: [NO_ERRORS_SCHEMA],
+            }).compileComponents();
+
+            fixture = TestBed.createComponent(TextboxFormComponent);
+            testComponent = fixture.componentInstance;
+            fixture.detectChanges();
+        });
+
+        describe("reactive form > ", () => {
             it("shouldn't emit valueChanges on blur", () => {
                 const callback = () => {};
                 const spy = jasmine.createSpy("valueChangesSpy", callback).and.callThrough();
-                const textbox = testFormComponent.input.textboxInput.nativeElement as HTMLInputElement;
+                const textbox = testComponent.input.textboxInput.nativeElement as HTMLInputElement;
 
-                testFormComponent.form.valueChanges.subscribe(spy);
+                testComponent.form.valueChanges.subscribe(spy);
 
                 textbox.dispatchEvent(new Event("focus"));
                 textbox.dispatchEvent(new Event("blur"));
