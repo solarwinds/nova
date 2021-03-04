757d98f3f8964146be71c956ddd34d5c495d35a0
Blake Simmon <blake.simmon@solarwinds.com>
2021-03-04 09:33:52 -0600

    Minor gauge fix

--- packages/charts/src/renderers/bar/linear-gauge-thresholds-renderer.ts
+++ packages/charts/src/renderers/bar/linear-gauge-thresholds-renderer.ts
@@ -50,7 +50,7 @@ export class LinearGaugeThresholdsRenderer extends BarRenderer {
             .attr("cx", (d, i) => renderSeries.scales.x.convert(accessors?.data?.endX?.(d, i, dataSeries.data, dataSeries)))
             .attr("cy", (d, i) => renderSeries.scales.y.convert(accessors?.data?.endY?.(d, i, dataSeries.data, dataSeries)))
             .attr("r", 4)
-            .style("fill", (d, i) => `var(--nui-color-icon-${data[i].hit ? "light" : "default"})`)
+            .style("fill", (d, i) => `var(${data[i].hit ? "--nui-color-text-light" : "--nui-color-icon-default"})`)
             .style("stroke-width", 0);
     }
 
