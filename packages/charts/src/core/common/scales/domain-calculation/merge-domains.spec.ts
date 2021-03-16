77bb4b32095ef1f85ab607d37aa19da90afe2eb6
Baggio Shehadi <baggio.shehadi@solarwinds.com>
2021-03-16 10:35:21 -0500

    Updated unit test

--- packages/charts/src/core/common/scales/domain-calculation/merge-domains.spec.ts
+++ packages/charts/src/core/common/scales/domain-calculation/merge-domains.spec.ts
@@ -3,7 +3,17 @@ import { EMPTY_CONTINUOUS_DOMAIN } from "../types";
 
 import { mergeDomains } from "./merge-domains";
 
-fdescribe("merge domain >", () => {
+describe("merge domain >", () => {
+
+    it("have a top and bottom domain", () => {
+        const scale = new LinearScale();
+
+        const domains = [[100, 2], [1, 101]];
+        const mergedDomains = mergeDomains(domains, scale);
+        expect(mergedDomains[0]).toEqual(1);
+        expect(mergedDomains[1]).toEqual(101);
+    });
+
     it("should skip domains that are equal to EMPTY_CONTINUOUS_DOMAIN", () => {
         const scale = new LinearScale();
 
