8c43335a08aa2039cd6bf1f96be9f377367191ae
Baggio Shehadi <70650726+baggiosw@users.noreply.github.com>
2021-03-16 13:46:21 -0500

    Bugfix/NUI-5866 timeseries widget empty data set domain (#176)
    * Added a filter on domains if it is empty skip the merge
    
    * Created unit test for function
    
    * Updating visual test to have a series with no data
    
    * Updated unit test
    
    * Changing name description
    
    * Added test to include an edge case

--- packages/charts/src/core/common/scales/domain-calculation/merge-domains.ts
+++ packages/charts/src/core/common/scales/domain-calculation/merge-domains.ts
@@ -2,7 +2,7 @@ import { max, min, Numeric } from "d3-array";
 import flatten from "lodash/flatten";
 import uniq from "lodash/uniq";
 
-import { IScale } from "../types";
+import { EMPTY_CONTINUOUS_DOMAIN, IScale } from "../types";
 
 /**
  * Merge given domains
@@ -12,8 +12,10 @@ import { IScale } from "../types";
  */
 export function mergeDomains(domains: any[][], scale: IScale<any>): any[] {
     if (scale.isContinuous()) {
-        const domainMin = min<Numeric>(domains.filter(e => typeof e[0] !== undefined).map(e => e[0]));
-        const domainMax = max<Numeric>(domains.filter(e => typeof e[1] !== undefined).map(e => e[1]));
+        const nonEmptyDomains = domains.filter(d => d !== EMPTY_CONTINUOUS_DOMAIN);
+
+        const domainMin = min<Numeric>(nonEmptyDomains.filter(e => typeof e[0] !== undefined).map(e => e[0]));
+        const domainMax = max<Numeric>(nonEmptyDomains.filter(e => typeof e[1] !== undefined).map(e => e[1]));
 
         return [domainMin, domainMax];
     }
