bbd8ebdb58a2b9c01f0a4fb6ea4142b80765222b
Dima Firsov <dmytro.firsov@solarwinds.com>
2021-03-16 17:04:21 +0200

    wip

--- packages/bits/spec/components/badge/badge.percy.ts
+++ packages/bits/spec/components/badge/badge.percy.ts
@@ -1,15 +1,21 @@
 import percySnapshot from "@percy/protractor";
-import { by, element, ElementFinder } from "protractor";
+import { browser } from "protractor";
+import fs from "fs";
 
 import { Animations, Helpers } from "../../helpers";
 
 const name: string = "Badge";
+const snapPath: string = "./spec/.snapshots/";
 
 describe(`Visual tests: PERCY ${name}`, () => {
 
     beforeAll(async () => {
         await Helpers.prepareBrowser("common/badge/badge-visual-test");
         await Helpers.disableCSSAnimations(Animations.ALL);
+
+        // if (!fs.existsSync(snapPath)) {
+        //     fs.mkdirSync(snapPath);
+        // }
     });
 
     afterAll(() => {
@@ -17,15 +23,16 @@ describe(`Visual tests: PERCY ${name}`, () => {
     });
 
     describe(`${name} - Test suite`, () => {
-        const el1: ElementFinder = element(by.tagName("body"));
-
-        it(`Default look`, async () => {
+        it(`${name} test`, async () => {
             await percySnapshot(`The ${name} Default Look`);
-        });
-
-        it(`Dark theme`, async () => {
+    
             await Helpers.switchDarkTheme("on");
             await percySnapshot(`The ${name} Dark Theme Look`);
+            // const png = await browser.takeScreenshot();
+            // fs.writeFileSync(snapPath + "badge-main.png", png, {encoding: "base64"});
+
+            // await Helpers.switchDarkTheme("on");
+            // fs.writeFileSync(snapPath + "badge-2.png", await browser.takeScreenshot(), {encoding: "base64"});
         });
     });
 });
