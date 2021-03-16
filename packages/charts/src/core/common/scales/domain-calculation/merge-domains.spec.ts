f763f7d9426ae023be1ab694690921afdb780531
Baggio Shehadi <baggio.shehadi@solarwinds.com>
2021-03-16 11:16:10 -0500

    Added test to include an edge case

--- packages/charts/src/core/common/scales/domain-calculation/merge-domains.spec.ts
+++ packages/charts/src/core/common/scales/domain-calculation/merge-domains.spec.ts
@@ -8,7 +8,7 @@ describe("merge domain >", () => {
     it("should grab the max and min out of a two element array", () => {
         const scale = new LinearScale();
 
-        const domains = [[100, 2], [1, 101]];
+        const domains = [EMPTY_CONTINUOUS_DOMAIN, [100, 2], [1, 101]];
         const mergedDomains = mergeDomains(domains, scale);
         expect(mergedDomains[0]).toEqual(1);
         expect(mergedDomains[1]).toEqual(101);
