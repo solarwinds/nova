c64b4c9f5c984a47517e5bff4a8a50927886de39
Podpryatova <nataliia.podpryatova@solarwinds.com>
2021-03-25 19:29:14 +0200

    fix according to comments

--- packages/bits/src/lib/selector/selector.component.ts
+++ packages/bits/src/lib/selector/selector.component.ts
@@ -105,6 +105,7 @@ export class SelectorComponent implements OnChanges, AfterViewInit, OnDestroy, I
         this.overlay.clickOutside
             .pipe(takeUntil(this.onDestroy$))
             .subscribe(_ => this.overlay.hide());
+        // TODO: should change programmatically in scope of NUI-5937
         this.checkbox.checkboxLabel.nativeElement.setAttribute("tabindex", "-1");
     }
 
