12a346ca05409da703a1b0b234a1c3b8c4aff186
dmytrognatyshyn <Dmytro.Gnatyshyn@solarwinds.com>
2021-04-12 17:40:40 +0300

    fixed quotes

--- packages/bits/demo/src/components/demo/textbox/textbox-visual-test/textbox-visual-test.component.ts
+++ packages/bits/demo/src/components/demo/textbox/textbox-visual-test/textbox-visual-test.component.ts
@@ -8,9 +8,9 @@ import { Component } from "@angular/core";
 export class TextboxVisualTestComponent {
     public isRequired = true;
     public value = "";
-    public textExample: string = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolores ' +
-        'ex harum itaque laborum laudantium quae quo, soluta suscipit voluptate. Accusamus aliquid amet architecto, ' +
-        'doloremque ducimus eveniet facere facilis fugit itaque';
+    public textExample: string = `Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolores
+        ex harum itaque laborum laudantium quae quo, soluta suscipit voluptate. Accusamus aliquid amet architecto
+        doloremque ducimus eveniet facere facilis fugit itaque`;
 
     public isInErrorState() {
         return this.isRequired && !this.value;
