5136fd335ec6ddec77a55a92a71148f758a8eb42
Dima Firsov <dmytro.firsov@solarwinds.com>
2021-04-15 18:36:36 +0300

    percy export fix

--- packages/bits/spec/virtual-camera/percy-lens.ts
+++ packages/bits/spec/virtual-camera/percy-lens.ts
@@ -1,10 +1,16 @@
 import { ProtractorBrowser } from "protractor";
 import { ICameraSettings, ILens, PERCY_DEFAULT_CONFIG } from "./types";
-import percySnapshot from "@percy/protractor";
-import { SnapshotOptions } from "@percy/core";
+
+export interface PercyLensSnapshotOptions {
+    widths?: number[];
+    minHeight?: number;
+    percyCSS?: string;
+    enableJavaScript?: boolean;
+  }
 
 export class PercyLens implements ILens {
-    private percyConfig: SnapshotOptions;
+    private percySnapshot = require("@percy/protractor");
+    private percyConfig: PercyLensSnapshotOptions;
 
     constructor(private browser: ProtractorBrowser, private settings: ICameraSettings) {
         this.percyConfig = { ...PERCY_DEFAULT_CONFIG };
@@ -13,7 +19,7 @@ export class PercyLens implements ILens {
     public async takeSnapshot(label: string): Promise<void> {
         this.checkSettings();
 
-        await percySnapshot(`${this.settings.currentTestName} - ${label}`, { ...this.percyConfig });
+        await this.percySnapshot(`${this.settings.currentTestName} - ${label}`, { ...this.percyConfig });
     }
 
     public async takeFullScreenSnapshot(label: string): Promise<void> {
@@ -24,7 +30,7 @@ export class PercyLens implements ILens {
 
     public async cameraOFF() {}
 
-    public toolConfig() { console.warn("No config is availalbe for Percy"); }
+    public toolConfig() { console.warn("No config is available for Percy"); }
 
     private checkSettings() {
         this.percyConfig.widths = [...this.settings.responsiveWidths];
