2f95817d621eaa20782f4aa7efb0d3ff58a9068e
Baggio Shehadi <baggio.shehadi@solarwinds.com>
2021-04-28 08:35:41 -0500

    Added unit test

---
+++ packages/dashboards/src/lib/configurator/components/widgets/table/columns-editor-v2/column-configuration/presentation-configuration/presentation-configuration-v2.component.spec.ts
@@ -0,0 +1,67 @@
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+
+import { IFormatterDefinition } from "../../../../../../../components/types";
+import { NuiDashboardsModule } from "../../../../../../../dashboards.module";
+import { ProviderRegistryService } from "../../../../../../../services/provider-registry.service";
+
+import { PresentationConfigurationV2Component } from "./presentation-configuration-v2.component";
+import { PIZZAGNA_EVENT_BUS } from "@nova-ui/dashboards";
+import { EventBus } from "@nova-ui/bits";
+import { AbstractControl, FormBuilder } from "@angular/forms";
+
+describe("PresentationConfigurationV2Component", () => {
+    let component: PresentationConfigurationV2Component;
+    let fixture: ComponentFixture<PresentationConfigurationV2Component>;
+    let formBuilder: FormBuilder;
+    const rawFormatter: IFormatterDefinition = {
+        componentType: "RawFormatterComponent",
+        label: $localize`No Formatter`,
+        dataTypes: {
+            // @ts-ignore: Suppressed for test purposes
+            value: null,
+        },
+    };
+    const linkFormatter: IFormatterDefinition = {
+        componentType: "LinkFormatterComponent",
+        label: $localize`Link`,
+        configurationComponent: "LinkConfiguratorComponent",
+        dataTypes: {
+            value: "label",
+            link: "link",
+        },
+    };
+
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            imports: [NuiDashboardsModule],
+            providers: [
+                ProviderRegistryService,
+                {
+                    provide: PIZZAGNA_EVENT_BUS,
+                    useClass: EventBus,
+                },
+            ],
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(PresentationConfigurationV2Component);
+        component = fixture.componentInstance;
+        formBuilder = new FormBuilder();
+        component.formControl = formBuilder.control({});
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    describe("propertiesForm >", () => {
+       it("should patch form when formatters are changed", () => {
+           component.formatters = [rawFormatter, linkFormatter];
+
+           component.ngOnInit();
+
+           expect(component.form.value.componentType).toEqual(rawFormatter.componentType)
+       });
+    });
+});
