d56fa4d6bfe290e75cfc00f9e4d25ea34b0d8b15
Vita-Karas <75837119+Vita-Karas@users.noreply.github.com>
2021-05-14 15:35:08 +0300

    bugfix/NUI-6038-Investigate-error-ExpressionChangedAfterItHasBeenCheckedError

--- packages/bits/src/lib/wizard-v2/wizard-footer.component.ts
+++ packages/bits/src/lib/wizard-v2/wizard-footer.component.ts
@@ -25,16 +25,16 @@ export class WizardFooterComponent implements FocusableOption, AfterViewInit, On
         private _elementRef: ElementRef<HTMLElement>
     ) {}
 
-    ngAfterViewInit() {
+    ngAfterViewInit(): void {
         this._focusMonitor.monitor(this._elementRef, true);
     }
 
-    ngOnDestroy() {
+    ngOnDestroy(): void {
         this._focusMonitor.stopMonitoring(this._elementRef);
     }
 
     /** Focuses the step footer. */
-    focus() {
+    focus(): void {
         this._focusMonitor.focusVia(this._elementRef, "program");
     }
 
