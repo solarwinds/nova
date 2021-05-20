289700e030e0a85c609775bf8aec254b33da921b
Dmytro Gnatyshyn <81757885+dmytrognatyshyn@users.noreply.github.com>
2021-05-20 10:49:29 +0300

    feature/ NUI-5993 Adding and removing dynamic step (#295)
    feature/ NUI-5993 Adding and removing dynamic step

--- packages/bits/demo/src/components/demo/wizard-v2/index.ts
+++ packages/bits/demo/src/components/demo/wizard-v2/index.ts
@@ -6,3 +6,4 @@ export * from "./wizard-busy/wizard-busy.example.component";
 export * from "./wizard-dialog/wizard-dialog.example.component";
 export * from "./wizard-custom-footer/wizard-custom-footer-example.component";
 export * from "./wizard-dynamic/wizard-dynamic.example.component";
+export * from "./wizard-remove-step/wizard-remove-step-example.component";
