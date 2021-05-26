af0bd70b71af8318fb7ca9179d6d4fa74be173bf
DmytroPutrin <82096709+DmytroPutrin@users.noreply.github.com>
2021-05-26 13:31:57 +0300

    Feature/n UI 3303 reset step (#302)
    * [NUI-3303] wizard.component.spec.ts add ut for reset method
    
    * [NUI-3303] wizard.component.ts add resetStep method
    
    * [NUI-3303] add wizard-reset-step-example
    
    * [NUI-3303] wizard-docs.example.component.html typo fix
    
    * [NUI-3303] wizard-reset-step-example.component.ts remove password validation
    
    * [NUI-3303] review: formatting

--- packages/bits/demo/src/components/demo/wizard/wizard-reset-step/wizard-reset-step-example.component.ts
+++ packages/bits/demo/src/components/demo/wizard/wizard-reset-step/wizard-reset-step-example.component.ts
@@ -1,18 +1,19 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { WizardComponent } from "@nova-ui/bits";
+import { IWizardSelectionEvent, WizardComponent } from "@nova-ui/bits";
 
 @Component({
-    selector: "nui-wizard-validation-example",
-    templateUrl: "./wizard-validation.example.component.html",
+    selector: "nui-wizard-reset-step-example",
+    templateUrl: "./wizard-reset-step.example.component.html",
 })
 
-export class WizardValidationExampleComponent implements OnInit {
+export class WizardResetStepExampleComponent implements OnInit {
     @ViewChild("wizardComponent") wizardComponent: WizardComponent;
     public myForm: FormGroup;
     public secondStepForm: FormGroup;
 
     constructor(private formBuilder: FormBuilder) {}
+
     public ngOnInit() {
         this.myForm = this.formBuilder.group({
             name: this.formBuilder.control("", Validators.required),
@@ -21,10 +22,7 @@ export class WizardValidationExampleComponent implements OnInit {
                 Validators.pattern("[^ @]*@[^ @]*"),
                 Validators.email,
             ]),
-            password: this.formBuilder.control("", [
-                Validators.required,
-                Validators.minLength(8),
-            ]),
+            password: this.formBuilder.control("", [Validators.required]),
         });
         this.secondStepForm = this.formBuilder.group({
             formCheckbox: [false, [Validators.requiredTrue]],
@@ -33,5 +31,16 @@ export class WizardValidationExampleComponent implements OnInit {
 
     public updateValidity() {
         this.secondStepForm.get("formCheckbox")?.updateValueAndValidity();
+    }
+
+    public onSelectionChange(event: IWizardSelectionEvent): void {
+        const { selectedIndex, previouslySelectedStep, previouslySelectedIndex } = event;
+
+        /* Example of reset statuses of second step */
+        if (selectedIndex === 0 && previouslySelectedIndex === 1) {
+            if (this.secondStepForm.invalid) {
+                this.wizardComponent.resetStep(previouslySelectedStep);
+            }
+        }
     }
 }
