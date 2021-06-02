de141a07428ca7d8b83200ccccc67763251aaeca
Lukas Holcik <lukas.holcik@solarwinds.com>
2021-06-02 15:27:40 -0500

    Added some API docs

--- packages/dashboards/src/lib/components/widget/widget-header/types.ts
+++ packages/dashboards/src/lib/components/widget/widget-header/types.ts
@@ -1,3 +1,11 @@
+/**
+ * The interface for a dynamic link provider for widget header.
+ */
 export interface IHeaderLinkProvider {
+    /**
+     * Generates a link url based on a user configured URL
+     *
+     * @param template a user configured URL
+     */
     getLink(template: string): string;
 }
