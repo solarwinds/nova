6f3c493895c7df59d4d114b5c8c3565086728e81
Lukáš Holčík <lukas.holcik@gmail.com>
2021-06-03 15:16:13 -0500

    Feature/n UI 6054 global filters in links (#318)
    * Dynamic link support
    
    * Removed element references
    
    * Removed console.log
    
    * Added some API docs
    
    * Change of name
    
    * prepareLink
    
    * PR comment fixes
    
    * Fixed provider injection token
    
    * Test + lint fix
    
    * Lint fix
    
    * Documentation
    
    * Documentation update

--- packages/dashboards/src/lib/components/widget/public-api.ts
+++ packages/dashboards/src/lib/components/widget/public-api.ts
@@ -2,3 +2,4 @@ export * from "./widget.component";
 export * from "./widget-header/widget-header.component";
 export * from "./widget-body/widget-body.component";
 export * from "./widget-body-content/widget-body-content.component";
+export * from "./widget-header/types";
