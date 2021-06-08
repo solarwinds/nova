6d69c41f24a1fe6a9dc19767aad26ff5aa6963d3
Lukas Holcik <lukas.holcik@solarwinds.com>
2021-06-08 13:45:49 -0500

    Removed fit

--- packages/charts/src/core/grid/config/dimension-config.spec.ts
+++ packages/charts/src/core/grid/config/dimension-config.spec.ts
@@ -2,7 +2,7 @@ import { DimensionConfig } from "@nova-ui/charts";
 
 describe("dimension-config", ()=> {
 
-    fit("doesn't allow for negative height to be set", ()=> {
+    it("doesn't allow for negative height to be set", ()=> {
         const dc = new DimensionConfig();
         dc.margin.top = 10;
         dc.margin.bottom = 10;
