e1a7408d72e79217894968c7459a819f8a961772
Madelyn Helton <madelyn.e.helton@gmail.com>
2021-06-14 16:50:38 -0500

    fixing unit test

--- packages/charts/src/core/common/scales/domain-calculation/merge-domains.spec.ts
+++ packages/charts/src/core/common/scales/domain-calculation/merge-domains.spec.ts
@@ -17,9 +17,10 @@ describe("merge domain >", () => {
     it("should skip domains that are equal to EMPTY_CONTINUOUS_DOMAIN", () => {
         const scale = new LinearScale();
 
-        const emptyArr = [EMPTY_CONTINUOUS_DOMAIN];
-        const mergedDomains = mergeDomains(emptyArr, scale);
-        expect(mergedDomains[0]).toBeDefined();
+        const validDomain = [0, 3];
+        const domains = [validDomain, EMPTY_CONTINUOUS_DOMAIN];
+        const mergedDomains = mergeDomains(domains, scale);
+        expect(mergedDomains).toEqual(validDomain);
     });
 
     it("should return continuous domain when the domains are undefined", () => {
