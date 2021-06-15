1859970fc84ad4a573a0abb83253f346c7856d32
Dima Firsov <dmytro.firsov@solarwinds.com>
2021-06-15 20:37:18 +0300

    tweaking the example to actually filter things

--- packages/bits/demo/src/components/demo/combobox-v2/combobox-v2-custom-typeahead/combobox-v2-custom-typeahead.example.component.ts
+++ packages/bits/demo/src/components/demo/combobox-v2/combobox-v2-custom-typeahead/combobox-v2-custom-typeahead.example.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { FormControl } from "@angular/forms";
-import { Observable } from "rxjs";
-import { map, startWith } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 interface IExampleItem {
     id: string;
@@ -12,31 +11,37 @@ interface IExampleItem {
     templateUrl: "combobox-v2-custom-typeahead.example.component.html",
     host: { class: "combobox-container" },
 })
-export class ComboboxV2CustomTypeaheadExampleComponent implements OnInit {
+export class ComboboxV2CustomTypeaheadExampleComponent {
     public items: IExampleItem[] = Array.from({ length: 100 }).map((_, i) =>
         ({
             id: `value-${i}`,
             name: $localize `Item ${i}`,
         }));
-    public comboboxControl = new FormControl();
-    public filteredItems$: Observable<any[]>;
 
-    ngOnInit() {
-        this.filteredItems$ = this.comboboxControl.valueChanges.pipe(
-            startWith(""),
-            map((value) => value?.name || value),
-            map((name) =>
-                name ? this.filterItems(name) : this.items.slice()
-            )
-        );
+    public comboboxControl = new FormControl();
+
+    // Use this in the template with async pipe to dynamically render the filtered items
+    public filteredItems$: Subject<any[]> = new Subject<any[]>();
+
+    public onValueChange(value: any): void {
+        // Please be aware that there is a known issue (NUI-6131) which results in the
+        // entire set of items appearing in the filtering results on input value set and change
+
+        // Once the combobox input changes the new value is emitted.
+        // Use this value to filter out the array of items
+        value
+            ? this.filteredItems$.next(this.filterItems(String(value)))
+            : this.filteredItems$.next(this.items.slice())
     }
 
     public displayFn(item: IExampleItem): string {
         return item?.name || "";
     }
 
+    // For the sake of the example, the filtering is quite simple.
+    // It filters out the combobox items depending on the user input.
     private filterItems(value: string): IExampleItem[] {
-        const filterValue = value.toLowerCase();
+        const filterValue = value.toLowerCase() || "";
 
         return this.items.filter(option => option.name.toLowerCase().includes(filterValue));
     }
