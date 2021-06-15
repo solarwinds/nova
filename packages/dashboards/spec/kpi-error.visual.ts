72b230c32fa80cef101eeb2772cf4aa46d02244a
Baggio Shehadi <70650726+baggiosw@users.noreply.github.com>
2021-06-15 10:26:20 -0500

    feature/nui 6020 finalize error handling component (#334)
    * Added a unit test
    
    * Added a few more unit tests
    
    * Fixed a unit test
    
    * fdescribe
    
    * added some documentation
    
    * Added visual tests for KPI error
    
    * removed fdescribe and uncommented back a camera
    
    * renamed selector
    
    * Lint error
    
    * Update packages/dashboards/examples/src/components/docs/tutorials/widget-error-handling/widget-error-handling-docs.component.html
    
    * Fix wording per Blake's comments
    
    * Changes per Blake's suggestions
    
    * removing an it to make visual test slightly faster
    
    Co-authored-by: Blake Simmon <Blakesalot@users.noreply.github.com>

---
+++ packages/dashboards/spec/kpi-error.visual.ts
@@ -0,0 +1,46 @@
+import { Atom, Camera } from "@nova-ui/bits/sdk/atoms";
+import { Helpers } from "@nova-ui/bits/sdk/atoms/helpers";
+import { browser, by, element } from "protractor";
+import { TestPage } from "./test.po";
+import { ConfiguratorAtom } from "./configurator/configurator.atom";
+
+const name: string = "Kpi Error";
+
+describe(`Visual tests: Dashboards - ${name}`, () => {
+    let camera: Camera;
+    let configurator: ConfiguratorAtom;
+    const page = new TestPage();
+
+    beforeAll(async () => {
+        await Helpers.prepareBrowser("test/kpi/error");
+        configurator = Atom.findIn(ConfiguratorAtom, element(by.className(ConfiguratorAtom.CSS_CLASS)));
+
+        camera = new Camera().loadFilm(browser, name);
+    });
+
+    it(`${name} - Configurator error message types`, async () => {
+        await camera.turn.on();
+
+        await camera.say.cheese(`${name} - Default`);
+
+        await page.enableEditMode();
+        await page.editWidget("Error Widget");
+
+        const dsAccordion = await configurator?.getAccordion("Value 1 - Average Rating", "Data Source");
+        await dsAccordion?.toggle();
+
+        await camera.say.cheese(`${name} - Error 0`);
+
+        const dataSourceSelect = dsAccordion?.getSelect("datasource-configuration__accordion-content__datasource-input");
+        await dataSourceSelect?.select("TestKpiDataSource2")
+        await camera.say.cheese(`${name} - Error 403`);
+
+        await dataSourceSelect?.select("TestKpiDataSourceBigNumber")
+        await camera.say.cheese(`${name} - Error 404`);
+
+        await dataSourceSelect?.select("TestKpiDataSourceSmallNumber")
+        await camera.say.cheese(`${name} - No Error`);
+
+        await camera.turn.off();
+    }, 100000);
+});
---
+++ packages/dashboards/src/lib/configurator/components/widgets/configurator-items/data-source-error/data-source-error.component.spec.ts
@@ -0,0 +1,108 @@
+import { DataSourceErrorComponent } from "./data-source-error.component";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { NuiDashboardsModule } from "../../../../../dashboards.module";
+import { DataSourceFeatures, IDataSource, IFilteringOutputs, IFilteringParticipants, NuiMessageModule } from "@nova-ui/bits";
+import { Subject } from "rxjs";
+import { SimpleChange } from "@angular/core";
+
+class MockDataSource implements IDataSource {
+    public outputsSubject = new Subject<IFilteringOutputs>();
+    public filterParticipants: IFilteringParticipants;
+    public features = new DataSourceFeatures({});
+
+    public async applyFilters(): Promise<void> {
+        // @ts-ignore: Suppressed for testing purposes
+        return null;
+    }
+
+    public registerComponent(components: Partial<IFilteringParticipants>): void {
+        // @ts-ignore: Suppressed for testing purposes
+        this.filterParticipants = components;
+    }
+
+    public deregisterComponent(componentKey: string) {
+        delete this.filterParticipants?.[componentKey];
+    }
+}
+
+describe("DataSourceErrorComponent", () => {
+    let component: DataSourceErrorComponent;
+    let fixture: ComponentFixture<DataSourceErrorComponent>;
+    let dataSource: IDataSource;
+
+    beforeEach(() => {
+        dataSource = new MockDataSource();
+
+        TestBed.configureTestingModule({
+            imports: [
+                NuiDashboardsModule,
+                NuiMessageModule,
+            ],
+        });
+        fixture = TestBed.createComponent(DataSourceErrorComponent);
+        component = fixture.componentInstance;
+
+        component.dataSource = dataSource;
+        const dataSourceChanged = { dataSource: new SimpleChange(null, dataSource, true) };
+        component.ngOnChanges(dataSourceChanged);
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it("should call changeDetector if there's an output on the dataSource", () => {
+        const markForCheckSpy = spyOn(component.changeDetector, "markForCheck");
+
+        component.dataSource.outputsSubject.next(null);
+
+        expect(markForCheckSpy).toHaveBeenCalled();
+    });
+
+    it("should emit an errorState positive if there's an error on the output of a dataSource", () => {
+        const errorStateSpy = spyOn(component.errorState, "emit");
+
+        component.dataSource.outputsSubject.next({
+            error: {
+                type: 404,
+                message: "Not Found",
+            },
+        });
+        expect(component.dataSourceError).toBeTruthy();
+
+        expect(errorStateSpy).toHaveBeenCalledWith(true);
+
+        component.dataSource.outputsSubject.next({
+            result: {
+                hello: "world",
+            },
+        });
+        expect(component.dataSourceError).toBeFalsy();
+        expect(errorStateSpy).toHaveBeenCalledWith(false);
+    });
+
+    it("should set data to the same value if there is a result key for legacy dataSources", () => {
+        component.dataSource.outputsSubject.next({
+            result: {
+                hello: "world",
+            },
+        });
+        expect(component.dataSourceError).toBeFalsy();
+
+        const testCaseA = component.data;
+        component.dataSource.outputsSubject.next({
+            hello: "world",
+        });
+        expect(component.dataSourceError).toBeFalsy();
+
+        const testCaseB = component.data;
+        expect(testCaseA).toEqual(testCaseB);
+
+        component.dataSource.outputsSubject.next({
+            hello: "new world",
+        });
+        const testCaseC = component.data;
+        expect(component.dataSourceError).toBeFalsy();
+        expect(testCaseC).not.toEqual(testCaseA);
+    });
+});
