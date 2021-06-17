f57378d6b89d627ce55747f6b6bec3f09dffcee6
Dima Firsov <dmytro.firsov@solarwinds.com>
2021-06-17 17:53:09 +0300

    second part of the problem

--- packages/bits/src/lib/menu/menu-key-control.service.ts
+++ packages/bits/src/lib/menu/menu-key-control.service.ts
@@ -102,7 +102,6 @@ export class MenuKeyControlService implements OnDestroy {
     }
 
     private handleOpenKeyDown(event: KeyboardEvent): void {
-        event.stopPropagation();
 
         if (event.code === KEYBOARD_CODE.ARROW_DOWN || event.code === KEYBOARD_CODE.ARROW_UP) {
             // passing the event to key manager so we get a change fired
