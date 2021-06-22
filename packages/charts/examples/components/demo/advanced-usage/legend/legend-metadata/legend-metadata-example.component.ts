d5571daf11e49683c39e40a12a3ed2544730cf37
Baggio Shehadi <baggio.shehadi@solarwinds.com>
2021-06-22 13:24:42 -0500

    Added a new example with metadata legend

---
+++ packages/charts/examples/components/demo/advanced-usage/legend/legend-metadata/legend-metadata-example.component.ts
@@ -0,0 +1,158 @@
+import { Component, OnInit } from "@angular/core";
+import {
+    areaGrid,
+    AreaRenderer,
+    Chart,
+    ChartAssist,
+    IAccessors,
+    IAreaAccessors,
+    IChartAssistSeries,
+    IChartSeries,
+    ILineAccessors,
+    IXYScales,
+    LineAccessors,
+    LinearScale,
+    LineRenderer,
+    stackedArea,
+    stackedAreaAccessors,
+    TimeScale,
+} from "@nova-ui/charts";
+import moment from "moment/moment";
+
+@Component({
+    selector: "area-chart-stack-example",
+    templateUrl: "./legend-metadata-example.component.html",
+})
+export class LegendMetadataExampleComponent implements OnInit {
+    public chart: Chart;
+    public chartAssist: ChartAssist;
+    public avgSeries: IChartAssistSeries<ILineAccessors>
+
+    public ngOnInit() {
+        // areaGrid returns an XYGrid configured for displaying an area chart's axes and other grid elements.
+        this.chart = new Chart(areaGrid());
+        // ChartAssist will use the preprocessor to stack the series' numeric values on the same progression domain
+        this.chartAssist = new ChartAssist(this.chart, stackedArea);
+
+        // Stacked Area accessors let the renderer know how to access x and y domain data respectively from a chart's input data set(s).
+        const accessors =  stackedAreaAccessors();
+        // 'y1' defines access to the numeric values we want to visualize.
+        // The items in the data array of this example have a property named 'value',so we'll use that.
+        accessors.data.y1 = (d) => d.value;
+
+        // The area renderer will make the chart look like a area chart.
+        const renderer = new AreaRenderer();
+
+        // In case of a area chart, the scale definitions are flexible.
+        // This example demonstrates a scenario with time on the X scale and a numeric value on the Y scale.
+        const scales: IXYScales = {
+            x: new TimeScale(),
+            y: new LinearScale(),
+        };
+
+        const averageData = getAverage(getData());
+        // We are using a different renderer so the metadata does not effect the domain
+        const noopRenderer = new LineRenderer({ignoreForDomainCalculation: true});
+        // Here we create an accessor for our average metadata
+        const avgAccessors = new LineAccessors();
+        // This is so the legend knows the value for the x
+        avgAccessors.data.x = (d) => d.x;
+        // This is so the legend knows the value for the y
+        avgAccessors.data.y = (d) => d.value;
+        // Same as above we are setting the numeric value we want to visualize
+        avgAccessors.data.y1 = avgAccessors.data.y;
+        // Setting the color of the series to transparent so it doesn't render
+        avgAccessors.series.color = () => "var(--nui-color-bg-transparent)";
+        // Deleting the markers so it doesn't place a marker on the chart
+        delete avgAccessors.series.marker;
+
+        this.avgSeries = {
+            ...averageData,
+            accessors: avgAccessors,
+            renderer: noopRenderer,
+            scales: scales,
+            showInLegend: false,
+            preprocess: false,
+        }
+        // Here we assemble the complete chart series.
+        let seriesSet: Partial<IChartSeries<IAccessors>>[] = getData().map(d => ({
+            ...d,
+            accessors,
+            renderer,
+            scales,
+        }));
+        // Combining both sets in an array of series
+        seriesSet = [...seriesSet, this.avgSeries];
+
+        // Finally, pass the series set to the chart's update method
+        this.chartAssist.update(seriesSet as IChartSeries<IAreaAccessors>[]);
+    }
+}
+
+function getAverage(dataArr: any) {
+    let arrAverage = [];
+    const dataLength =  dataArr[0].data.length;
+    const numOfSeries = dataArr.length
+    for (let n = 0; n < dataLength; n++) {
+        let avg = 0;
+        for (let i = 0; i < dataArr.length; i++) {
+            const series = dataArr[i];
+            avg += series.data[n].value;
+        }
+        avg = avg/numOfSeries
+        arrAverage.push({ x: dataArr[0].data[n].x, value: avg });
+    }
+    return {
+        id: "average",
+        name: "Average Speed",
+        data: arrAverage
+    };
+}
+
+/* Chart data */
+function getData() {
+    const format = "YYYY-MM-DDTHH:mm:ssZ";
+
+    return [
+        {
+            id: "series-1",
+            name: "Series 1",
+            data: [
+                { x: moment("2016-12-25T11:45:29.909Z", format), value: 6 },
+                { x: moment("2016-12-25T12:10:29.909Z", format), value: 33 },
+                { x: moment("2016-12-25T12:50:29.909Z", format), value: 15 },
+                { x: moment("2016-12-25T13:15:29.909Z", format), value: 20 },
+                { x: moment("2016-12-25T13:40:29.909Z", format), value: 30 },
+                { x: moment("2016-12-25T13:55:29.909Z", format), value: 12 },
+                { x: moment("2016-12-25T14:20:29.909Z", format), value: 6 },
+                { x: moment("2016-12-25T14:40:29.909Z", format), value: 35 },
+                { x: moment("2016-12-25T15:00:29.909Z", format), value: 23 },
+                { x: moment("2016-12-25T15:25:29.909Z", format), value: 25 },
+                { x: moment("2016-12-25T15:45:29.909Z", format), value: 38 },
+                { x: moment("2016-12-25T16:10:29.909Z", format), value: 25 },
+                { x: moment("2016-12-25T16:30:29.909Z", format), value: 43 },
+                { x: moment("2016-12-25T16:45:29.909Z", format), value: 28 },
+            ],
+        },
+        {
+            id: "series-2",
+            name: "Series 2",
+            data: [
+                { x: moment("2016-12-25T11:45:29.909Z", format), value: 12 },
+                { x: moment("2016-12-25T12:10:29.909Z", format), value: 65 },
+                { x: moment("2016-12-25T12:50:29.909Z", format), value: 30 },
+                { x: moment("2016-12-25T13:15:29.909Z", format), value: 40 },
+                { x: moment("2016-12-25T13:40:29.909Z", format), value: 60 },
+                { x: moment("2016-12-25T13:55:29.909Z", format), value: 23 },
+                { x: moment("2016-12-25T14:20:29.909Z", format), value: 12 },
+                { x: moment("2016-12-25T14:40:29.909Z", format), value: 70 },
+                { x: moment("2016-12-25T15:00:29.909Z", format), value: 45 },
+                { x: moment("2016-12-25T15:25:29.909Z", format), value: 50 },
+                { x: moment("2016-12-25T15:45:29.909Z", format), value: 75 },
+                { x: moment("2016-12-25T16:10:29.909Z", format), value: 50 },
+                { x: moment("2016-12-25T16:30:29.909Z", format), value: 85 },
+                { x: moment("2016-12-25T16:45:29.909Z", format), value: 55 },
+            ],
+        },
+    ];
+}
