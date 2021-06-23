3da69ef1813bd31cddaea02d9c9eb169fa620a78
Baggio Shehadi <baggio.shehadi@solarwinds.com>
2021-06-23 15:40:49 -0500

    Added a generic renderer and accessor

--- packages/charts/src/renderers/accessors/xy-accessors.ts
+++ packages/charts/src/renderers/accessors/xy-accessors.ts
@@ -1,4 +1,6 @@
-import { DataAccessor, IAccessors } from "../../core/common/types";
+import { DataAccessor, IAccessors, SeriesAccessor } from "../../core/common/types";
+import { defaultColorProvider, defaultMarkerProvider } from "../../core/common/palette/default-providers";
+
 
 export interface IXYDataAccessors {
     /** Accessor for value plotted on the <code>x</code> coordinate */
@@ -8,6 +10,19 @@ export interface IXYDataAccessors {
     /** Additional custom keys to match the base interface */
     [key: string]: DataAccessor | undefined;
 }
+export interface IXYSeriesAccessors {
+    /** Color of the series */
+    color?: SeriesAccessor;
+    /** Marker for the series */
+    marker?: SeriesAccessor;
+    /** Additional custom keys to match the base interface */
+    [key: string]: SeriesAccessor | undefined;
+}
+export interface IXYAccessors extends IAccessors {
+    data: IXYDataAccessors;
+    /** Series level accessors - e.g. for colors, markers, etc. */
+    series: IXYSeriesAccessors;
+}
 
 export class XYAccessors implements IAccessors {
     /** The default data accessors for using with renderers deriving from XYRenderer */
@@ -15,4 +30,15 @@ export class XYAccessors implements IAccessors {
         x: (d: any) => d.x,
         y: (d: any) => d.y,
     };
+    public series: IXYSeriesAccessors;
+
+    constructor(public colorProvider = defaultColorProvider(), public markerProvider = defaultMarkerProvider()) {
+
+        this.data.defined = (d: any, i) => d.hasOwnProperty("defined") ? d.defined : true;
+
+        this.series = {
+            color: this.colorProvider ? this.colorProvider.get : undefined,
+            marker: this.markerProvider ? this.markerProvider.get : undefined,
+        };
+    }
 }
