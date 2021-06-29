d16de133eb1844fb1d5710bd60321b40a0705c56
DmytroPutrin <dmytro.putrin@solarwinds.com>
2021-06-29 14:49:16 +0300

    NUI-5070 wizard-step.components.spec.ts ut for isErrorState method

--- packages/bits/src/lib/wizard-v2/wizard-step/wizard-step.components.spec.ts
+++ packages/bits/src/lib/wizard-v2/wizard-step/wizard-step.components.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { WizardStepHeaderComponent } from "../wizard-step-header/wizard-step-header.component";
 import { CdkStep, CdkStepper } from "@angular/cdk/stepper";
 import { WizardDirective } from "../wizard.directive";
+import { FormControl, NgForm } from "@angular/forms";
 
 describe("components >", () => {
     describe("WizardStepComponent", () => {
@@ -32,6 +33,30 @@ describe("components >", () => {
 
                 component.ngOnInit();
                 expect(spy).toHaveBeenCalled();
+            });
+        });
+
+        describe("isErrorSep", () => {
+            const control = { invalid: false } as FormControl;
+            const form = {} as NgForm;
+
+            it("should call _errorStateMatcher 'isErrorState' method", () => {
+                const spy = spyOn(component["_errorStateMatcher"], "isErrorState" as never);
+
+                component.isErrorState(control, form);
+                expect(spy).toHaveBeenCalledWith(control as never, form as never);
+            });
+
+            it("should return true if control invalid and errorState is true", () => {
+                spyOn(component["_errorStateMatcher"], "isErrorState").and.returnValue(true);
+
+                expect(component.isErrorState({ invalid: true } as FormControl, form)).toBeTrue();
+            });
+
+            it("should return false if control valid and errorState is false", () => {
+                spyOn(component["_errorStateMatcher"], "isErrorState").and.returnValue(false);
+
+                expect(component.isErrorState(control, form)).toBeFalse();
             });
         });
     });
