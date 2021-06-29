3f8d2d16a5bf8e7203fb07223a190a19628488d1
DmytroPutrin <dmytro.putrin@solarwinds.com>
2021-06-29 10:22:25 +0300

    NUI-5070 linter fix

--- packages/bits/src/lib/wizard-v2/wizard-footer/wizard-footer.component.spec.ts
+++ packages/bits/src/lib/wizard-v2/wizard-footer/wizard-footer.component.spec.ts
@@ -1,5 +1,5 @@
 import { WizardFooterComponent } from "./wizard-footer.component";
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 describe("components >", () => {
     describe("WizardFooterComponent", () => {
