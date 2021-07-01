4b060cd10dc98e08ce6093f0db10128602f83545
Madelyn Helton <madelyn.e.helton@gmail.com>
2021-07-01 09:58:48 -0500

    lint fix

--- packages/bits/spec/components/table/table.visual.ts
+++ packages/bits/spec/components/table/table.visual.ts
@@ -92,7 +92,8 @@ describe(`Visual tests: ${name}`, () => {
 
         await expanders.virtualScrollStickyHeader.click();
         await camera.say.cheese("Virtual Scroll Table with Sticky Header");
-        await browser.executeScript("document.getElementById('table-virtual-scroll-sticky-header-test').getElementsByClassName('nui-table__container')[0].scrollTop = '20'");
+        await browser.executeScript(
+            "document.getElementById('table-virtual-scroll-sticky-header-test').getElementsByClassName('nui-table__container')[0].scrollTop = '20'");
         await camera.say.cheese("Virtual Scroll Table with Sticky Header - scrolled");
         await expanders.virtualScrollStickyHeader.click();
 
