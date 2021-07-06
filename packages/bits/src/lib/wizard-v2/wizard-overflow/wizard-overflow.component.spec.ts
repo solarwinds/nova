d9c4e17e112dbe4fc73a2c99aaa9a9db644628cb
Dima Firsov <dmytro.firsov@solarwinds.com>
2021-07-06 12:25:29 +0300

    lintfix

--- packages/bits/src/lib/wizard-v2/wizard-overflow/wizard-overflow.component.spec.ts
+++ packages/bits/src/lib/wizard-v2/wizard-overflow/wizard-overflow.component.spec.ts
@@ -1,25 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { WizardOverflowComponent } from './wizard-overflow.component';
+import { WizardOverflowComponent } from "./wizard-overflow.component";
 
-describe('WizardOverflowComponent', () => {
-  let component: WizardOverflowComponent;
-  let fixture: ComponentFixture<WizardOverflowComponent>;
+describe("WizardOverflowComponent", () => {
+    let component: WizardOverflowComponent;
+    let fixture: ComponentFixture<WizardOverflowComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ WizardOverflowComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [ WizardOverflowComponent ],
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(WizardOverflowComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(WizardOverflowComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
