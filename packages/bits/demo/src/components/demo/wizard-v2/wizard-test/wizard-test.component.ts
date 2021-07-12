9b5ec7418630961c2efd0cad05252e253e3d634d
DmytroPutrin <dmytro.putrin@solarwinds.com>
2021-07-12 18:54:50 +0300

    NUI-6084 wizard-test.component.html add examples

--- packages/bits/demo/src/components/demo/wizard-v2/wizard-test/wizard-test.component.ts
+++ packages/bits/demo/src/components/demo/wizard-v2/wizard-test/wizard-test.component.ts
@@ -1,10 +1,108 @@
 import {
-    Component,
+    Component, Inject, TemplateRef,
 } from "@angular/core";
+import { Subject } from "rxjs";
+import { FlexibleConnectedPositionStrategy, OverlayRef } from "@angular/cdk/overlay";
+import { DialogService, NuiDialogRef, OverlayComponent, WizardHorizontalComponent } from "@nova-ui/bits";
+import { takeUntil } from "rxjs/operators";
 
 @Component({
     selector: "nui-wizard-test",
     templateUrl: "./wizard-test.component.html",
+    styleUrls: ["./wizard-test.less"],
 })
 export class WizardTestComponent {
+    public onDestroy$ = new Subject<void>();
+    public overlayTriggered$ = new Subject<void>();
+
+    private overlayRef: OverlayRef;
+    public busy: boolean = false;
+    public activeDialog: NuiDialogRef;
+
+    constructor(@Inject(DialogService) private dialogService: DialogService) {}
+
+    public vegetables = [$localize `Cabbage`, $localize `Potato`, $localize `Tomato`, $localize `Carrot`];
+    public selectedVegetables = [$localize `Potato`, $localize `Tomato`];
+
+    public isChecked(vegetable: string): boolean {
+        return this.selectedVegetables.indexOf(vegetable) > -1;
+    }
+
+    public valuesChanged(values: any[]): void {
+        this.selectedVegetables = [...values];
+    }
+
+    public openDialog(content: TemplateRef<string>): void {
+        this.activeDialog = this.dialogService.open(content, {
+            size: "lg",
+            windowClass: "active-dialog",
+        });
+    }
+
+    public closeDialog(): void {
+        this.activeDialog.close();
+    }
+
+    public toggleBusy(): void {
+        this.busy = !this.busy;
+        setTimeout(() => {
+            this.busy = false;
+        }, 3000);
+    }
+
+    // Open confirmation overlay
+    public openConfirmationOverlay(overlay: OverlayComponent, wizard: WizardHorizontalComponent): void {
+        if (wizard.selectedIndex === 0) {
+            this.closeDialog();
+            return;
+        }
+
+        this.overlayTriggered$.next();
+
+        // Toggling the overlay to get an access to the 'overlayRef'
+        overlay.toggle();
+
+        this.overlayRef = overlay.getOverlayRef();
+        // Here we update the positions for the overlay. By default, the overlay service connects the overlay to the bottom-left
+        // corner of the toggle reference element. We change this behavior here by setting new positions, since we want overlay to
+        // stick to the upper-top corner of the parent container
+        (this.overlayRef.getConfig().positionStrategy as FlexibleConnectedPositionStrategy).withPositions([{
+            originX: "start",
+            originY: "top",
+            overlayX: "start",
+            overlayY: "top",
+        }]);
+        // We update the size of the overlay container to follow the dimensions of the new 'toggle reference' container we set in the very first step
+        this.updateOverlayDimensions(overlay);
+
+        // Handling ESC events inside overlay
+        this.activeDialog?.closed$.pipe(
+            takeUntil(this.overlayTriggered$),
+            takeUntil(this.onDestroy$)).subscribe(() => overlay.hide());
+    }
+
+    public open(content: TemplateRef<string>): void {
+        this.activeDialog = this.dialogService.open(content, {size: "lg", backdrop: "static", useOverlay: true});
+    }
+
+    public actionDone(): void {
+        this.activeDialog.close();
+    }
+
+    public onContainerResize(overlay: OverlayComponent): void {
+        this.updateOverlayDimensions(overlay);
+    }
+
+    public ngOnDestroy(): void {
+        this.onDestroy$.next();
+        this.onDestroy$.complete();
+        this.overlayTriggered$.complete();
+    }
+
+    private updateOverlayDimensions(overlay: OverlayComponent): void {
+        this.overlayRef?.updateSize({
+            width: overlay.toggleReference.getBoundingClientRect().width,
+            height: overlay.toggleReference.getBoundingClientRect().height,
+        });
+    }
 }
