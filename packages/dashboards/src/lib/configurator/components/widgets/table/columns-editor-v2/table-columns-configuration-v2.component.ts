237a88bb86c6b3fbd76c9c9c2ab1350d21fd35ac
Stepan Stava <stepan.stava@solarwinds.com>
2021-10-05 10:13:21 +0200

    NUI-6158-Formatters-are-not-validated-when-slightly-changing-query (#407)

--- packages/dashboards/src/lib/configurator/components/widgets/table/columns-editor-v2/table-columns-configuration-v2.component.ts
+++ packages/dashboards/src/lib/configurator/components/widgets/table/columns-editor-v2/table-columns-configuration-v2.component.ts
@@ -13,7 +13,7 @@ import {
     SimpleChanges,
     ViewEncapsulation,
 } from "@angular/core";
-import { FormArray, FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup } from "@angular/forms";
 import { DialogService, EventBus, IDataField, IDataSource, IEvent, uuid } from "@nova-ui/bits";
 import isUndefined from "lodash/isUndefined";
 import values from "lodash/values";
@@ -153,6 +153,16 @@ export class TableColumnsConfigurationV2Component implements OnInit, IHasForm, O
         cols.controls = columns.map(c => {
             const fc = new FormControl(c);
             fc.setParent(cols);
+
+            // Nova's markAsTouched function gets attached to formControl during init of TableColumnConfigurationComponent,
+            // must be pass to newly created formControl here otherwise gets lost and column validation doesn't get triggered.
+            const colControl = cols.controls.find(
+                (cControl: AbstractControl) => cControl.value.id === c.id
+            );
+            if (colControl) {
+                fc.markAsTouched = colControl.markAsTouched;
+            }
+
             return fc;
         });
         cols.updateValueAndValidity({ emitEvent });
