fc7efbd57da8f8d4bcb3c963d8a9db55cbfedf19
Stepan Stava <stepan.stava@solarwinds.com>
2021-10-19 14:14:58 +0200

    removes invalid property from the check

--- packages/dashboards/src/lib/configurator/functions/has-control-in-error-state.ts
+++ packages/dashboards/src/lib/configurator/functions/has-control-in-error-state.ts
@@ -1,8 +1,11 @@
 import { AbstractControl, FormArray, FormGroup } from "@angular/forms";
 
 /**
- * Checks whether given form has a field that should be visually marked as invalid
- * Considers a FormGroup/FormArray subform tree without an error if all child forms are either valid or untouched
+ * Checks whether given form has a field that should be visually marked as invalid.
+ * Form is not in an error state if it's untouched even if it's invalid at that moment,
+ * a user must first interact with the form before it can be considered as invalid.
+ * 'invalid' form property should not be used for the purpose of this function, 
+ * it can be affected by its children form statuses, which can be considered as valid (untouched).
  *
  * @param parent
  */
@@ -12,6 +15,7 @@ export function hasControlInErrorState(parent: AbstractControl): boolean {
         return false;
     }
 
+    // FormGroup is considred valid if every child form is either valid or untouched
     if (parent instanceof FormGroup) {
         for (const key of Object.keys(parent.controls)) {
             const control = parent.controls[key];
@@ -20,10 +24,9 @@ export function hasControlInErrorState(parent: AbstractControl): boolean {
                 return true;
             }
         }
-        // Any error hasn't been found in FormGroup subtree, the whole FormGroup is considered without an error
-        return false;
     }
 
+    // FormArray is considred valid if every child form is either valid or untouched
     if (parent instanceof FormArray) {
         for (let i = 0; i < parent.length; i++) {
             const control = parent.at(i);
@@ -32,10 +35,8 @@ export function hasControlInErrorState(parent: AbstractControl): boolean {
                 return true;
             }
         }
-        // Any error hasn't been found in FormArray subtree, the whole FormArray is considered without an error
-        return false;
     }
 
-    // FormControl is in an error state if it's touched/dirty and invalid
-    return ((parent.touched || parent.dirty) && parent.invalid);
+    // Errors property contains only errors related to the currecnt form and it's not affected by any child forms
+    return ((parent.touched || parent.dirty) && !!parent.errors);
 }
