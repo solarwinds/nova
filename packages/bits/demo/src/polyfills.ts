1b8e12fa8eda0bd057cf42a83e74ba861c712f64
Evžen Zelinka <mizuki.nakeshu@gmail.com>
2021-11-19 18:30:33 +0100

    (upgrade) ng12

--- packages/bits/demo/src/polyfills.ts
+++ packages/bits/demo/src/polyfills.ts
@@ -47,7 +47,7 @@ import "core-js/es7/reflect";
  * Zone JS is required by default for Angular itself.
  */
 // tslint:disable-next-line:ordered-imports
-import "zone.js/dist/zone";  // Included with Angular CLI.
+import "zone.js";  // Included with Angular CLI.
 
 
 
--- packages/bits/src/test.ts
+++ packages/bits/src/test.ts
@@ -1,6 +1,6 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
-import "zone.js/dist/zone-testing"; // this needs to be first
+import "zone.js/testing"; // this needs to be first
 // eslint-disable-next-line
 import { getTestBed } from "@angular/core/testing";
 import {
