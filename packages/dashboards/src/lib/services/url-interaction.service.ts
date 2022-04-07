4783ac6c0f1bc21d1d661147e7f415a41a79f5e1
Erik <erik.hudcovsky@solarwinds.com>
2022-04-07 09:29:16 +0200

    NUI-6169 Fix lint errors

--- packages/dashboards/src/lib/services/url-interaction.service.ts
+++ packages/dashboards/src/lib/services/url-interaction.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core";
 
 @Injectable({
-    providedIn: 'root'
+    providedIn: "root",
 })
 export class UrlInteractionService {
     public template(url: string, data: any): string{
