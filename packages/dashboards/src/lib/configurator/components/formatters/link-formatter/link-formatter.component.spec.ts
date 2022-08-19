d89b33ecc6d2507fbda4ca809e9d6ed6ac62fb4d
Evžen Zelinka <evzen.zelinka@solarwinds.com>
2022-08-19 12:16:58 +0200

    (fix) link formatter - update config property name

--- packages/dashboards/src/lib/configurator/components/formatters/link-formatter/link-formatter.component.spec.ts
+++ packages/dashboards/src/lib/configurator/components/formatters/link-formatter/link-formatter.component.spec.ts
@@ -1,32 +1,45 @@
-import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+
 import { LinkFormatterComponent } from "./link-formatter.component";
 
-describe("LinkFormatterComponent", () => {
+describe(LinkFormatterComponent.name, () => {
     let component: LinkFormatterComponent;
     let fixture: ComponentFixture<LinkFormatterComponent>;
 
-    beforeEach(waitForAsync(() => {
+    beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [
                 LinkFormatterComponent,
-            ],
-            imports: [],
-            providers: [
             ],
         }).compileComponents();
 
         fixture = TestBed.createComponent(LinkFormatterComponent);
         component = fixture.componentInstance;
-    }));
+        component.data = {
+            link: "http://some.fake/link",
+            value: "text",
+        };
+    });
+
+    const getLink = (): DebugElement | null => fixture.debugElement.query(By.css("a"));
+
+    const assertLinkTarget = (expectedTarget: string) => {
+        const link = getLink();
+        expect(link?.attributes.target).toBe(expectedTarget);
+    };
 
     it("Defaults to open in new tab", () => {
         component.ngOnChanges({});
-        expect(component.target).toBe("_blank");
+        fixture.detectChanges();
+        assertLinkTarget("_blank");
     });
 
     it("Detects changes for target property", () => {
         component.targetSelf = true;
         component.ngOnChanges({});
-        expect(component.target).toBe("_self");
+        fixture.detectChanges();
+        assertLinkTarget("_self");
     });
 });
