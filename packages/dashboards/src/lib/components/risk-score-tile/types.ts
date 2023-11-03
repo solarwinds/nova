92baed8a60f9736611a2519dac5a4bf0a0fca740
swi-global-circleci <66067460+swi-global-circleci@users.noreply.github.com>
2023-11-03 16:07:49 +0100

    Update release files v12.0.41-0 (#659)
    * increment develop versions
    
    * OO-8569 fix remove button perfstack summary view (#571)
    
    Add remove button to xy chart
    
    * Hotfix - merging changes (#573)
    
    * update files for version
    
    * increment develop versions
    
    ---------
    
    Co-authored-by: Evžen Zelinka <evzen.zelinka@solarwinds.com>
    
    * Force update of CODEOWNERS file (#620)
    
    CODEOWNERS update due to team renaming
    
    Co-authored-by: codeowners-change-solarwinds[bot] <137421838+codeowners-change-solarwinds[bot]@users.noreply.github.com>
    
    * bugfix/OO-22514 stacked area chart missing remove btn (#627)
    
    * update d3 lib versions according to d3 lib versions in Fusion (#623)
    
    * update d3 library from v5 to v7
    
    * Update zoom-plugin.ts
    
    * d3 events replaced
    
    * Update mouse-interactive-area.ts
    
    * Update mouse-interactive-area.ts
    
    * Update mouse-interactive-area.ts
    
    * Update mouse-interactive-area.ts
    
    * Update mouse-interactive-area.ts
    
    * remove other imports
    
    * Update mouse-interactive-area.ts
    
    * Update mouse-interactive-area.ts
    
    * update types of d3
    
    * Update package.json
    
    * Update yarn.lock
    
    * remove older versions
    
    * update d3-selection-multi
    
    * downgrade types d3
    
    * downgrade types
    
    * update types
    
    * return peer dependencies
    
    * downgrade d3-selection types
    
    * add removed packages
    
    * remove everything
    
    * add selection multi
    
    * add selection multi
    
    * Update bar-renderer.ts
    
    * Update package.json
    
    * Update bar-renderer.ts
    
    * downgrade selection multi
    
    * return selection multi to the original value
    
    * complete revert
    
    * update selection multi
    
    * update d3
    
    * magic
    
    * downgrade
    
    * Update mouse-interactive-area.ts
    
    * Update timeseries-zoom-plugin.ts
    
    * Update package.json
    
    * remove refactoring
    
    * Increment develop versions to 12.0.38 0 (#629)
    
    * update files for version
    
    * Release - merging changes (#572)
    
    * increment develop versions
    
    * OO-8569 fix remove button perfstack summary view (#571)
    
    Add remove button to xy chart
    
    * update files for version
    
    ---------
    
    Co-authored-by: Evžen Zelinka <evzen.zelinka@solarwinds.com>
    
    * increment develop versions
    
    ---------
    
    Co-authored-by: Evžen Zelinka <evzen.zelinka@solarwinds.com>
    Co-authored-by: Erik Hudcovský <47628511+erzik987@users.noreply.github.com>
    
    * NUI-6193 Fix timepicker on examples (#632)
    
    * bugfix/OO-22938 - Fusion Migration - Perfstack - Stacked Percent Area Chart (#633)
    
    * fix: legend values
    
    * Update yarn.lock
    
    * Increment develop v12.0.39 (#636)
    
    * Improve release script (#647)
    
    * fix: added fastpoll icon (#649)
    
    * increment develop versions (#651)
    
    Co-authored-by: solarwinds <circleci@circleci>
    
    * Bugfix/oo 24328 policy compliance chart issues (#652)
    
    * fix: stacked area domain calculation
    
    * fix: changed summary serie tile color
    
    * fix: added missing colors
    
    * fix: formatting
    
    * feature/OO-25095 Add blue color variable (#653)
    
    * increment develop versions (#655)
    
    Co-authored-by: solarwinds <circleci@circleci>
    
    * Add RiskScore Component to the Bits package and Risk Score widget type to the Dashboard package #639 (#656)
    
    * Add RiskScore Component to the Bits package
    
    * Add Risk Score Widget
    
    * Add Risk Score Widget Configuration Components
    
    * update files for version 12.0.41-0
    
    ---------
    
    Co-authored-by: Evžen Zelinka <evzen.zelinka@solarwinds.com>
    Co-authored-by: Erik Hudcovský <47628511+erzik987@users.noreply.github.com>
    Co-authored-by: codeowners-change-solarwinds[bot] <137421838+codeowners-change-solarwinds[bot]@users.noreply.github.com>
    Co-authored-by: pilat-martin <97229167+pilat-martin@users.noreply.github.com>
    Co-authored-by: Michal Klein <kleinmichalgm@gmail.com>
    Co-authored-by: Filip Listiak <filip.listiak@solarwinds.com>
    Co-authored-by: solarwinds <circleci@circleci>
    Co-authored-by: Mykhailo Molyboha <40260984+MykhailoMolyboha@users.noreply.github.com>

---
+++ packages/dashboards/src/lib/components/risk-score-tile/types.ts
@@ -0,0 +1,58 @@
+// © 2023 SolarWinds Worldwide, LLC. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import { IFormatter } from "../types";
+
+export enum RiskScoreFormatterTypes {
+    // ROOT ?
+    Value = "Value",
+}
+
+export interface IRiskScoreData {
+    id?: string;
+    value?: number;
+    minValue?: number;
+    maxValue?: number;
+    label?: string;
+    description?: string;
+    backgroundColor?: string;
+    textColor?: string;
+    fontSize?: string;
+    numberFormat?: string;
+    link?: string;
+    [key: string]: any;
+}
+
+export interface IRiskScoreConfiguration {
+    interactive?: boolean;
+    formatters?: IRiskScoreFormattersConfiguration;
+}
+
+export interface IRiskScoreFormattersConfiguration
+    extends Partial<
+        Record<
+            RiskScoreFormatterTypes | string,
+            {
+                formatter: IFormatter;
+            }
+        >
+    > {}
+export interface IRiskScoreFormatterProperties
+    extends Partial<Record<RiskScoreFormatterTypes | string, any>> {}
