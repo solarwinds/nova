e9da686bd13a0bc6b0932e15c80a87e387f6c2a9
pavlo.poimanov <pavlo.poimanov@solarwinds.com>
2024-01-18 12:21:18 +0200

    (feat) Add demo files generation charts

---
+++ packages/charts/examples/src/demo-files-factory.ts
@@ -0,0 +1,14 @@
+import { DEMO_PATHS } from "./components/demo/demo.files";
+
+export const getDemoFiles = (filePrefix: string): {context: string; files: {content: string; path: string}[]} => ({
+        context: filePrefix,
+        files: DEMO_PATHS
+            .filter(filePath => filePath.includes(filePrefix))
+            .map(filePath => ({
+                content: require(`!!raw-loader!./components/demo/${filePath}`).default,
+                path: filePath,
+            }))
+            .concat({content: require(`./../../package.json.raw!=!raw-loader!./../../package.json`).default, path: "package.json"})})
+
+
+
