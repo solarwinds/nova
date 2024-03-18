115b8351a6b57dc14f153667a81e95d3a7e06af9
Erik Hudcovský <erik.hudcovsky@solarwinds.com>
2024-03-18 13:55:35 +0100

    NUI-6198 fix scroll dismiss event

--- packages/bits/src/lib/dialog/dialog.component.ts
+++ packages/bits/src/lib/dialog/dialog.component.ts
@@ -35,8 +35,8 @@ import {
     Renderer2,
     ViewEncapsulation,
 } from "@angular/core";
-import { Router } from "@angular/router";
-import { take } from "rxjs/operators";
+import { NavigationEnd, Router } from "@angular/router";
+import { filter, map } from "rxjs/operators";
 
 const FOCUSABLE_SELECTOR =
     "a, button, input, textarea, select, details, [tabindex]:not([tabindex='-1'])";
@@ -151,9 +151,11 @@ export class DialogComponent implements OnInit, AfterViewInit, OnDestroy {
             this.ngZone
         );
         this.scrollDispatcher.register(this.scrollableElement);
-        this.router.events.pipe(take(1)).subscribe(() => {
-            this.dismiss("ROUTE_CHANGED");
-        });
+        this.router.events.pipe(
+            filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+            map(e => {
+                this.dismiss("ROUTE_CHANGED");
+            }));
     }
 
     public ngAfterViewInit(): void {
