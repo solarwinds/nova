93334fab26cb08537127aafd2ddb0ffd08f06fd8
pavlo <pauluxxx746@gmail.com>
2025-04-11 14:11:34 +0300

    lint dashboards errors

--- packages/dashboards/src/lib/configurator/services/widget-cloner.service.ts
+++ packages/dashboards/src/lib/configurator/services/widget-cloner.service.ts
@@ -48,7 +48,7 @@ export class WidgetClonerService {
                 ) => {
                     componentRef.instance.cloneSelectionComponentType =
                         cloner.widgetSelectionComponentType;
-                    componentRef.instance.changeDetector?.detectChanges()
+                    componentRef.instance.changeDetector?.detectChanges();
                     setTimeout(() => {
                         this.ref = componentRef;
                     });
