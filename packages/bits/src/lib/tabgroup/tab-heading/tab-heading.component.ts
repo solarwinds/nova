0b943882ca0ffbc7c83755d358890cc00c83b55e
Tomas Nemec <tomas.nemec@solarwinds.com>
2025-09-11 09:03:03 +0200

    fix(tab-heading): prevent default click behavior on tab headings

--- packages/bits/src/lib/tabgroup/tab-heading/tab-heading.component.ts
+++ packages/bits/src/lib/tabgroup/tab-heading/tab-heading.component.ts
@@ -79,7 +79,6 @@ export class TabHeadingComponent {
 
     public onKeyDown(event: KeyboardEvent): void {
         if (event.key === "Enter" || event.key === " ") {
-            event.preventDefault();
             this.elementRef.nativeElement.click();
         }
     }
