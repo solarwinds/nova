a61976b4e0bc4d0a3d686781181a25bf4c76b5c2
Erik Hudcovský <47628511+erzik987@users.noreply.github.com>
2025-10-01 12:55:30 +0200

    Add color picker to bits in v17 (#783)
    * copy color-picker from kpi
    
    * Add select color picker
    
    * A
    add tests + refactor
    
    * Refactor less and html
    
    * remove comment
    
    * PR review fixes
    
    * Updated changelog
    
    * update changelog for v17
    
    * final pr fixes

--- packages/bits/demo/src/components/demo/color-picker/color-picker-select/color-picker-select.example.component.ts
+++ packages/bits/demo/src/components/demo/color-picker/color-picker-select/color-picker-select.example.component.ts
@@ -18,46 +18,34 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-import { Injectable } from "@angular/core";
-import isNil from "lodash/isNil";
+import { Component } from "@angular/core";
+import {
+    FormBuilder,
+    FormControl,
+    FormGroup,
+} from "@angular/forms";
+import { HTML_COLORS, IPaletteColor } from "../../../../../../src/constants/color-picker.constants";
 
-import { HTML_COLORS } from "./HTMLcolors";
 
-@Injectable()
-export class ColorService {
-    public RGBToHexRGBToHex(rgbColor: string): string {
-        // Choose correct separator
-        const sep = rgbColor.indexOf(",") > -1 ? "," : " ";
-        // Turn "rgb(r,g,b)" into [r,g,b]
-        const rgbParts = rgbColor.substr(4).split(")")[0].split(sep);
+@Component({
+    selector: "nui-color-picker-select-example",
+    templateUrl: "./color-picker-select.example.component.html",
+    styles: [],
+    standalone: false,
+})
+export class ColorPickerSelectExampleComponent {
+    public myForm: FormGroup<{ backgroundColor: FormControl<string | null> }>;
+    public colorPalette: IPaletteColor[] = Array.from(HTML_COLORS.entries())
+    .map(([label, color]) => ({label,color}));
+  
+    
+    constructor(
+        private formBuilder: FormBuilder
+    ) {}
 
-        let r = (+rgbParts[0]).toString(16),
-            g = (+rgbParts[1]).toString(16),
-            b = (+rgbParts[2]).toString(16);
-
-        if (r.length === 1) {
-            r = "0" + r;
-        }
-        if (g.length === 1) {
-            g = "0" + g;
-        }
-        if (b.length === 1) {
-            b = "0" + b;
-        }
-
-        return "#" + r + g + b;
-    }
-
-    public isHEX(hexColor: string): boolean {
-        return (
-            /^#[0-9A-F]{6}$/i.test(hexColor) ||
-            /^#([0-9A-F]{3}){1,2}$/i.test(hexColor)
-        );
-    }
-
-    public colorKeywordsToHex(color: string): string {
-        const hexOfColor = HTML_COLORS.get(color);
-
-        return !isNil(hexOfColor) ? hexOfColor : "";
+    public ngOnInit(): void {
+        this.myForm = this.formBuilder.group({
+            backgroundColor: [""],
+        });
     }
 }
