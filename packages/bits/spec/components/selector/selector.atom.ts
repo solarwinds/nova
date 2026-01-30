46dd9a7f26bec67fcf45e47abafb6ccc535a30d7
pavlo poimanov <pavlo.poimanov@solarwinds.com>
2026-01-30 16:09:12 +0200

    spec protractor

--- packages/bits/spec/components/selector/selector.atom.ts
+++ packages/bits/spec/components/selector/selector.atom.ts
@@ -1,99 +1,99 @@
-// // © 2022 SolarWinds Worldwide, LLC. All rights reserved.
-// //
-// // Permission is hereby granted, free of charge, to any person obtaining a copy
-// //  of this software and associated documentation files (the "Software"), to
-// //  deal in the Software without restriction, including without limitation the
-// //  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-// //  sell copies of the Software, and to permit persons to whom the Software is
-// //  furnished to do so, subject to the following conditions:
-// //
-// // The above copyright notice and this permission notice shall be included in
-// //  all copies or substantial portions of the Software.
-// //
-// // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// //  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// //  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// //  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// //  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// //  THE SOFTWARE.
+// © 2022 SolarWinds Worldwide, LLC. All rights reserved.
 //
-// import { browser, by, ElementFinder, ExpectedConditions } from "protractor";
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
 //
-// import { Atom } from "../../atom";
-// import { ButtonAtom } from "../button/button.atom";
-// import { CheckboxAtom } from "../checkbox/checkbox.atom";
-// import { MenuPopupAtom } from "../menu-popup/menu-popup.atom";
-// import { PopupAtom } from "../popup/popup.atom";
+// The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
 //
-// export enum SelectionType {
-//     All = "Select all items on this page",
-//     UnselectAll = "Unselect all items on this page",
-//     None = "Unselect all items",
-//     AllPages = "Select all items on all pages",
-// }
-//
-// export class SelectorAtom extends Atom {
-//     public static CSS_CLASS = "nui-selector";
-//
-//     public getPopupAtom(): PopupAtom {
-//         return Atom.findIn(PopupAtom, this.getElement());
-//     }
-//
-//     public async select(selectionType: SelectionType): Promise<void> {
-//         if (!(await this.isOpened())) {
-//             await this.getToggle().click();
-//         }
-//         await this.clickItemByText(selectionType);
-//     }
-//
-//     public async selectAppendedToBodyItem(
-//         selectionType: SelectionType
-//     ): Promise<void> {
-//         if (!(await this.getPopupAtom().isOpenedAppendToBody())) {
-//             await this.getToggle().click();
-//         }
-//         const menuPopup = Atom.findIn(
-//             MenuPopupAtom,
-//             this.getElement().element(by.className("nui-menu-popup"))
-//         );
-//         await browser.wait(
-//             ExpectedConditions.stalenessOf(menuPopup.getElement()),
-//             3000 // this is mostly for table. 1000ms may be not enough
-//         );
-//         await this.clickAppendedToBodyItemByText(selectionType);
-//     }
-//
-//     public getCheckbox(): CheckboxAtom {
-//         return Atom.findIn(CheckboxAtom, this.getElement());
-//     }
-//
-//     public getSelectorButton(): ButtonAtom {
-//         const buttonContainer = super
-//             .getElement()
-//             .element(by.className("nui-selector__checkbox-button"));
-//         return Atom.findIn(ButtonAtom, buttonContainer);
-//     }
-//
-//     public async clickItemByText(title: string): Promise<void> {
-//         await Atom.findIn(
-//             MenuPopupAtom,
-//             this.getElement().element(by.className("nui-menu-popup"))
-//         ).clickItemByText(title);
-//     }
-//
-//     public async clickAppendedToBodyItemByText(title: string): Promise<void> {
-//         await Atom.findIn(
-//             MenuPopupAtom,
-//             browser.element(by.css("body .nui-overlay .nui-menu-popup"))
-//         ).clickItemByText(title);
-//     }
-//
-//     private getToggle(): ElementFinder {
-//         return this.getElement().element(by.css(".nui-selector__toggle"));
-//     }
-//
-//     private async isOpened(): Promise<boolean> {
-//         return this.getPopupAtom().isOpened();
-//     }
-// }
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import { browser, by, ElementFinder, ExpectedConditions } from "protractor";
+
+import { Atom } from "../../atom";
+import { ButtonAtom } from "../button/button.atom";
+import { CheckboxAtom } from "../checkbox/checkbox.atom";
+import { MenuPopupAtom } from "../menu-popup/menu-popup.atom";
+import { PopupAtom } from "../popup/popup.atom";
+
+export enum SelectionType {
+    All = "Select all items on this page",
+    UnselectAll = "Unselect all items on this page",
+    None = "Unselect all items",
+    AllPages = "Select all items on all pages",
+}
+
+export class SelectorAtom extends Atom {
+    public static CSS_CLASS = "nui-selector";
+
+    public getPopupAtom(): PopupAtom {
+        return Atom.findIn(PopupAtom, this.getElement());
+    }
+
+    public async select(selectionType: SelectionType): Promise<void> {
+        if (!(await this.isOpened())) {
+            await this.getToggle().click();
+        }
+        await this.clickItemByText(selectionType);
+    }
+
+    public async selectAppendedToBodyItem(
+        selectionType: SelectionType
+    ): Promise<void> {
+        if (!(await this.getPopupAtom().isOpenedAppendToBody())) {
+            await this.getToggle().click();
+        }
+        const menuPopup = Atom.findIn(
+            MenuPopupAtom,
+            this.getElement().element(by.className("nui-menu-popup"))
+        );
+        await browser.wait(
+            ExpectedConditions.stalenessOf(menuPopup.getElement()),
+            3000 // this is mostly for table. 1000ms may be not enough
+        );
+        await this.clickAppendedToBodyItemByText(selectionType);
+    }
+
+    public getCheckbox(): CheckboxAtom {
+        return Atom.findIn(CheckboxAtom, this.getElement());
+    }
+
+    public getSelectorButton(): ButtonAtom {
+        const buttonContainer = super
+            .getElement()
+            .element(by.className("nui-selector__checkbox-button"));
+        return Atom.findIn(ButtonAtom, buttonContainer);
+    }
+
+    public async clickItemByText(title: string): Promise<void> {
+        await Atom.findIn(
+            MenuPopupAtom,
+            this.getElement().element(by.className("nui-menu-popup"))
+        ).clickItemByText(title);
+    }
+
+    public async clickAppendedToBodyItemByText(title: string): Promise<void> {
+        await Atom.findIn(
+            MenuPopupAtom,
+            browser.element(by.css("body .nui-overlay .nui-menu-popup"))
+        ).clickItemByText(title);
+    }
+
+    private getToggle(): ElementFinder {
+        return this.getElement().element(by.css(".nui-selector__toggle"));
+    }
+
+    private async isOpened(): Promise<boolean> {
+        return this.getPopupAtom().isOpened();
+    }
+}
---
+++ packages/bits/spec/components/textbox/textbox.e2e.ts
@@ -0,0 +1,179 @@
+// © 2022 SolarWinds Worldwide, LLC. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import {
+    browser,
+    by,
+    element,
+    ElementFinder,
+    ExpectedConditions,
+} from "protractor";
+
+import { Atom } from "../../atom";
+import { Helpers } from "../../helpers";
+import {
+    CheckboxAtom,
+    CheckboxGroupAtom,
+    SpinnerAtom,
+    TextboxAtom,
+} from "../public_api";
+
+xdescribe("USERCONTROL textbox >", () => {
+    let textBox: TextboxAtom;
+    let disabledTextbox: TextboxAtom;
+    let readonlyTextbox: TextboxAtom;
+    let customWidthTextbox: TextboxAtom;
+    let requiredTextbox: TextboxAtom;
+    let asyncSpinner: SpinnerAtom;
+    let asyncCheckbox: CheckboxAtom;
+    let checkboxGroup: CheckboxGroupAtom;
+    let validationMessage: ElementFinder;
+    let validationMessageText: ElementFinder;
+
+    beforeAll(async () => {
+        await Helpers.prepareBrowser("textbox");
+        asyncCheckbox = Atom.find(CheckboxAtom, "nui-demo-async-checkbox");
+        textBox = Atom.find(TextboxAtom, "demo-options-textbox-item");
+        disabledTextbox = Atom.find(
+            TextboxAtom,
+            "demo-options-disabled-textbox-item"
+        );
+        requiredTextbox = Atom.find(
+            TextboxAtom,
+            "demo-options-required-textbox-item"
+        );
+        readonlyTextbox = Atom.find(
+            TextboxAtom,
+            "demo-options-readonly-textbox-item"
+        );
+        customWidthTextbox = Atom.find(
+            TextboxAtom,
+            "demo-options-custom-width-textbox-item"
+        );
+        asyncSpinner = Atom.findIn(SpinnerAtom, element(by.id("test-textbox")));
+        checkboxGroup = Atom.findIn(
+            CheckboxGroupAtom,
+            element.all(by.className("demo-options-section")).first()
+        );
+        validationMessage = element(by.className("nui-textbox__messages"));
+        validationMessageText = element(
+            by.css(".nui-textbox__messages.nui-validation > div")
+        );
+    });
+
+    it(`should have native input element without "disabled" attribute by default`, async () => {
+        expect(await textBox.disabled()).toBe(false);
+    });
+
+    it(`should have native input element with "disabled" attribute`, async () => {
+        expect(await disabledTextbox.disabled()).toBe(true);
+    });
+
+    it(`should have native input element with "readonly" attribute`, async () => {
+        expect(await readonlyTextbox.isReadonly()).toBe(true);
+    });
+
+    it("allows to change width", async () => {
+        const baseWidth = (await customWidthTextbox.input.getSize()).width;
+        expect(baseWidth).toEqual(200);
+    });
+
+    describe("validation 'required' >", () => {
+        it("should reflect error when required field is empty", async () => {
+            expect(await requiredTextbox.hasError()).toBe(true);
+            await requiredTextbox.acceptText("42");
+            expect(await requiredTextbox.hasError()).toBe(false);
+        });
+    });
+
+    // TODO: disabled, since validation messages are moved to separate component and cannot be used outside of form. To be changed (delete/move)
+    xdescribe("testing sync and async validation", () => {
+        beforeEach(async () => {
+            if ((await asyncCheckbox.isChecked()) === false) {
+                await asyncCheckbox.toggle();
+            }
+
+            // Validation is not triggered by default
+            expect(await validationMessage.isPresent()).toBe(false);
+        });
+
+        it("should spinner be visible during the async validate of the textbox", async () => {
+            await textBox.clearText();
+            await textBox.acceptText("abcde");
+            await asyncSpinner.waitForDisplayed(5000);
+            expect(await asyncSpinner.isDisplayed()).toBe(true);
+        });
+
+        it("should prevent async validation if sync is triggered first", async () => {
+            await textBox.clearText();
+            await textBox.acceptText("Ted");
+            expect(await validationMessage.isDisplayed()).toBe(true);
+            expect(await validationMessageText.getText()).not.toBe(
+                "Ted's error"
+            );
+        });
+
+        it("should trigger sync validation on invalid value", async () => {
+            await textBox.clearText();
+            await textBox.acceptText("1");
+            await browser.sleep(2000);
+            expect(await asyncSpinner.isDisplayed()).toBe(false);
+            await browser.wait(
+                ExpectedConditions.visibilityOf(validationMessage),
+                2000
+            );
+            expect(await validationMessage.isDisplayed()).toBe(true);
+        });
+
+        it("should not trigger sync validation if user clears optional field", async () => {
+            await textBox.clearText();
+            await textBox.acceptText("1");
+            await textBox.deleteTextManually();
+            await asyncSpinner.waitForDisplayed(2000);
+            expect(await asyncSpinner.isDisplayed()).toBe(true);
+            await browser.sleep(2000);
+            expect(await validationMessage.isPresent()).toBe(false);
+        });
+
+        it("should show error message if async validation fails as user types letters in", async () => {
+            // Checking the min length checkbox off for the test to get rid off sync validation
+            await checkboxGroup.getFirst().toggle();
+            await textBox.clearText();
+            const ted = "Ted".split("");
+
+            // Simulating user's behavior by entering each letter one by one and checking the validation state
+            for (let idx = 0; idx < ted.length; idx++) {
+                const letter = ted[idx];
+                await textBox.acceptText(letter);
+                await asyncSpinner.waitForDisplayed(1000);
+                if (idx !== ted.length - 1) {
+                    await browser.sleep(2000);
+                    expect(await validationMessage.isPresent()).toBe(false);
+                } else {
+                    await browser.sleep(2000);
+                    expect(await validationMessage.isDisplayed()).toBe(true);
+                    expect(await validationMessageText.getText()).toBe(
+                        "Ted's error"
+                    );
+                }
+            }
+        });
+    });
+});
