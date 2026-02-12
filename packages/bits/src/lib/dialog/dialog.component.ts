e2fc80175c0fbdad77281b53f25f0e61e6f86fb2
harsha573252 <harshavardhan5757@gmail.com>
2026-02-12 17:49:59 +0530

    bits-build fix

--- packages/bits/src/lib/dialog/dialog.component.ts
+++ packages/bits/src/lib/dialog/dialog.component.ts
@@ -18,7 +18,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 
-import { CdkScrollable, ScrollDispatcher } from "@angular/cdk/scrolling";
+import { CdkScrollable } from "@angular/cdk/scrolling";
 import { DOCUMENT } from "@angular/common";
 import {
     AfterViewInit,
