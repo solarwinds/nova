fc994c2a3eda341f54866d5e20a0f9954a939de9
Oleksandr Nechyporenko <oleksandr.nechyporenko@solarwinds.com>
2026-03-24 18:35:31 +0100

    [OO-53003] Adding UT

---
+++ packages/dashboards/src/lib/components/timeseries-widget/timeseries-helpers.spec.ts
@@ -0,0 +1,98 @@
+import { ITimeseriesWidgetData } from "./types";
+import { metricsSeriesMeasurementsMinMax } from "./timeseries-helpers";
+
+describe("metricsSeriesMeasurementsMinMax", () => {
+    function makeSeries(
+        overrides: Partial<ITimeseriesWidgetData> = {}
+    ): ITimeseriesWidgetData {
+        return {
+            id: "s1",
+            name: "Series 1",
+            description: "",
+            data: [],
+            metricUnits: undefined,
+            ...overrides,
+        };
+    }
+
+    describe("when axisUnits is percent", () => {
+        it("should return { min: 0, max: 100 } when all values are <= 100", () => {
+            const series = [
+                makeSeries({
+                    metricUnits: "percent",
+                    data: [
+                        { x: 1, y: 50 },
+                        { x: 2, y: 80 },
+                    ],
+                }),
+            ];
+            expect(metricsSeriesMeasurementsMinMax(series, "percent")).toEqual({
+                min: 0,
+                max: 100,
+            });
+        });
+
+        it("should return max > 100 when percent data exceeds 100", () => {
+            const series = [
+                makeSeries({
+                    metricUnits: "percent",
+                    data: [
+                        { x: 1, y: 50 },
+                        { x: 2, y: 150 },
+                    ],
+                }),
+            ];
+            expect(metricsSeriesMeasurementsMinMax(series, "percent")).toEqual({
+                min: 0,
+                max: 150,
+            });
+        });
+
+        it("should return { min: 0, max: 100 } when there are no percent metrics", () => {
+            const series = [
+                makeSeries({
+                    metricUnits: "bytes",
+                    data: [
+                        { x: 1, y: 500 },
+                    ],
+                }),
+            ];
+            expect(metricsSeriesMeasurementsMinMax(series, "percent")).toEqual({
+                min: 0,
+                max: 100,
+            });
+        });
+
+        it("should return { min: 0, max: 100 } when percent metrics have no data", () => {
+            const series = [
+                makeSeries({
+                    metricUnits: "percent",
+                    data: [],
+                }),
+            ];
+            expect(metricsSeriesMeasurementsMinMax(series, "percent")).toEqual({
+                min: 0,
+                max: 100,
+            });
+        });
+
+        it("should consider the highest value across multiple percent series", () => {
+            const series = [
+                makeSeries({
+                    id: "s1",
+                    metricUnits: "percent",
+                    data: [{ x: 1, y: 120 }],
+                }),
+                makeSeries({
+                    id: "s2",
+                    metricUnits: "percent",
+                    data: [{ x: 1, y: 200 }],
+                }),
+            ];
+            expect(metricsSeriesMeasurementsMinMax(series, "percent")).toEqual({
+                min: 0,
+                max: 200,
+            });
+        });
+    });
+});
