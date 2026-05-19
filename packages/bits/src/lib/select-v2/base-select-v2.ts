17333348a7a8035d10ea40354b6307be427d6514
ludmila-fialova_swi <ludmila.fialova@solarwinds.com>
2026-05-20 00:12:34 +0200

    fixed bug and lint

--- packages/bits/src/lib/select-v2/base-select-v2.ts
+++ packages/bits/src/lib/select-v2/base-select-v2.ts
@@ -87,6 +87,8 @@ export abstract class BaseSelectV2
         OnDestroy,
         OnChanges
 {
+    private static _counter = 0;
+
     /** Value used as a placeholder for the select. */
     @Input() public placeholder: string = "";
 
@@ -195,7 +197,6 @@ export abstract class BaseSelectV2
 
     /** Unique ID for the trigger element, used for aria-labelledby on the overlay */
     public readonly triggerId = `nui-select-trigger-${BaseSelectV2._counter++}`;
-    private static _counter = 0;
     private virtualScrollResizeObserver: ResizeObserver;
 
     /** Emits value which has been selected */
