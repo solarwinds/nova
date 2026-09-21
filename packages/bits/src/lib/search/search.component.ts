2335a0390f11ea6c4b496e140ad785c7322607ab
novik.vitalii <novickvitaliy@gmail.com>
2026-09-21 14:07:10 +0300

    fix

--- packages/bits/src/lib/search/search.component.ts
+++ packages/bits/src/lib/search/search.component.ts
@@ -145,9 +145,13 @@ export class SearchComponent implements IFilterPub {
     public onCancel(): void {
         this.value.set("");
         this.cancel.emit(this.value());
-        this.captureFocus.set(true);
-        this.focusChange.emit(true);
+        this.captureFocus.set(false);
+        queueMicrotask(() => {
+            this.captureFocus.set(true);
+            this.focusChange.emit(true);
+        });
     }
+
     public onFocusChange(event: boolean): void {
         this.captureFocus.set(event);
         this.focusChange.emit(event);
