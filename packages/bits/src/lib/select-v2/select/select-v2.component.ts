37569c58c2c01f6e703073d9f299d19cecb9ca9f
novik.vitalii <novickvitaliy@gmail.com>
2026-09-21 12:19:08 +0300

    fix tests

--- packages/bits/src/lib/select-v2/select/select-v2.component.ts
+++ packages/bits/src/lib/select-v2/select/select-v2.component.ts
@@ -68,9 +68,8 @@ import { OptionKeyControlService } from "../option-key-control.service";
     ],
     changeDetection: ChangeDetectionStrategy.OnPush,
     host: {
-        "class": "nui-select-v2",
-        "role": "button",
-        "[attr.aria-label]": "accessibleLabel",
+        class: "nui-select-v2",
+        role: "none",
     },
     standalone: false,
 })
@@ -141,19 +140,13 @@ export class SelectV2Component
     /** Sets value to the model */
     public writeValue(value: OptionValueType | OptionValueType[]): void {
         super.writeValue(value);
-        if (!this.multiselect) {
-            this.defineDisplayText();
-        }
+        this.defineDisplayText();
         this.cdRef.markForCheck();
     }
 
     /** Checks whether value of the Select is empty */
     public get isEmpty(): boolean {
         return !this.getLastSelectedOption();
-    }
-
-    public get accessibleLabel(): string | null {
-        return this.ariaLabel || this.displayText || this.placeholder || null;
     }
 
     /**
